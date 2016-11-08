ccef0de1d73b55c95c00e99a32cdfeeb8a22fd7c
Key <key.hui@iheha.com>
2016-11-08 18:09:37 +0800

    no message

--- app/src/main/java/app/keyfun/youtubeplayerdemo/MainActivity.java
+++ app/src/main/java/app/keyfun/youtubeplayerdemo/MainActivity.java
@@ -2,12 +2,50 @@ package app.keyfun.youtubeplayerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.examples.youtubeapidemo.DeveloperKey;
+import com.examples.youtubeapidemo.YouTubeFailureRecoveryActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+public class MainActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener, YouTubePlayer.OnFullscreenListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.fragments_demo);
+
+        YouTubePlayerSupportFragment youTubePlayerFragment =
+                (YouTubePlayerSupportFragment) getSupportFragmentManager().findFragmentById(R.id.youtube_fragment);
+        youTubePlayerFragment.initialize(DeveloperKey.DEVELOPER_KEY, this);
+    }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerSupportFragment) getSupportFragmentManager().findFragmentById(R.id.youtube_fragment);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("nCgQDjiotG0");
+        }
+        player.setFullscreen(false);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Log.d(TAG, "onInitializationFailure = " + youTubeInitializationResult);
+    }
+
+    @Override
+    public void onFullscreen(boolean b) {
+        Log.d(TAG, "onFullscreen = " + b);
     }
 }
